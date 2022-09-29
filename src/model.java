c89a45395cfc9e35f44a99f1d793860cec795666
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-09-29 14:07:44 +0200

    kan läsa från en fil

--- src/model.java
+++ src/model.java
@@ -1,2 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class model {
+
+    public static void main(String[] args) throws IOException {
+        String input = new String();
+        FileReader file = new FileReader("input.txt");
+
+        BufferedReader bufferedreader = new BufferedReader(file);
+        Scanner scanner = new Scanner(bufferedreader);
+
+        while (scanner.hasNextLine()) {
+            input = input + scanner.nextLine();
+        }
+        bufferedreader.close();
+        System.out.println(input);
+    }
+
+
+
+
+    private static int encrypt(int input, int key) {
+        return (input^key);
+    }
 }
