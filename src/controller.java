3f306789f52ecd5cd465c26005ef8d0bce42b918
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-10-20 13:43:24 +0200

    Har skapat en veiw och model och lagt till knappar och textfält. nästa lektion lista ut hur man kopplar knapparna i view till controller och model.

--- src/controller.java
+++ src/controller.java
@@ -1,2 +1,39 @@
-public class controller {
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class controller extends JFrame {
+    model model;
+    gui gui;
+
+    public controller(model m, gui v) {
+        this.model = m;
+        this.gui = v;
+        this.setContentPane(gui.getPanel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        gui.setCryptListener(new cryptListener());
+        gui.setkeyListener(new cryptListener());
+        gui.setinputListener(new cryptListener());
+
+    }
+
+    private class cryptListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            model.setInput(gui.getMessage());
+            model.setKey(gui.getKey());
+            model.crypt_string();
+            gui.setCrypt(model.getCrypt());
+        }
+    }
+
+    public static void main(String[] args) {
+        model m = new model();
+        gui v = new gui();
+        controller thisIsTheProgram = new controller(m,v);
+        thisIsTheProgram.setVisible(true);
+
+    }
 }
--- src/gui.java
+++ src/gui.java
@@ -1,10 +1,50 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class gui {
-    private JTextField textField1;
-    private JButton button1;
-    private JTextField textField2;
-    private JButton inputKeyButton;
-    private JButton button3;
-    private JButton button4;
+    private JPanel panel;
+    private JTextField inputtextfield;
+    private JTextField inputkeyfield;
+    private JButton importKeyFromFileButton;
+    private JButton importTextFromInputButton;
+    private JButton exportToOutputTxtButton;
+    private JButton encryptDecryptButton;
+    private JTextArea outputcrypt;
+
+    public gui() {
+
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public String getMessage() {
+        return inputtextfield.getText();
+    }
+
+    public String getKey() {
+        return inputkeyfield.getText();
+    }
+
+    public void setCrypt(String c) {
+        outputcrypt.setText(c);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("View");
+        frame.setContentPane(new gui().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public void setCryptListener(ActionListener cryptListener) {
+        encryptDecryptButton.addActionListener(cryptListener);
+    }
+    public void setkeyListener(ActionListener keyListener) {
+        importKeyFromFileButton.addActionListener(keyListener);
+    }
+    public void setinputListener(ActionListener inputListener) {
+        importTextFromInputButton.addActionListener(inputListener);
+    }
 }
